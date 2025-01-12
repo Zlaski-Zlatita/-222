b5bc1748ea24fb7899ce3b89998950a46d94e357
Zlaski-Zlatita <169074132+Zlaski-Zlatita@users.noreply.github.com>
2025-01-12 11:10:42 +0000

    Create book_service.py

---
+++ app/services/book_service.py
@@ -0,0 +1,1 @@
+from app.database import get_session from app.models import Book async def get_books(session): return await session.query(Book).all() async def create_book(book, session): session.add(book) await session.commit() return book async def get_book(book_id, session): return await session.query(Book).get(book_id)
