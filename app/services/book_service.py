1ddbb9fef6e4d5a8c121046794e58cd895bc94cd
Zlaski-Zlatita <169074132+Zlaski-Zlatita@users.noreply.github.com>
2025-01-12 11:31:18 +0000

    Update book_service.py

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -1,1 +1,7 @@
-from app.database import get_session from app.models import Book async def get_books(session): return await session.query(Book).all() async def create_book(book, session): session.add(book) await session.commit() return book async def get_book(book_id, session): return await session.query(Book).get(book_id)
+from app.database import get_session
+from app.models import Book 
+
+async def get_books(session): 
+  return await session.query(Book).all() 
+
+async def create_book(book, session): session.add(book) await session.commit() return book async def get_book(book_id, session): return await session.query(Book).get(book_id)
